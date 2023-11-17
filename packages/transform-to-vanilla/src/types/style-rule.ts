dd0dc96ae840fd6484967c0f750ac6131a933abc
alstjr7375 <alstjr7375@daum.net>
2023-11-18 00:36:12 +0900

    Fix: Transform structure

--- packages/transform-to-vanilla/src/types/style-rule.ts
+++ packages/transform-to-vanilla/src/types/style-rule.ts
@@ -2,3 +2,6 @@ import type { ComplexStyleRule, StyleRule } from "@vanilla-extract/css";
 
 export type ComplexCSSRule = ComplexStyleRule;
 export type CSSRule = StyleRule;
+
+export type VanillaStyleArray = Exclude<ComplexStyleRule, StyleRule>;
+export type VanillaClassNames = Exclude<VanillaStyleArray[number], StyleRule>;
