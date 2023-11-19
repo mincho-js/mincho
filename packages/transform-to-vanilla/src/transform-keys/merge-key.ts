3775f2a7448ac2baf25a9fecaa2b67e215df4931
JeongJun <rgfdds98@gmail.com>
2023-11-19 15:56:26 +0900

    Test: add test case about Remove Key Sign Symbol Fn

--- packages/transform-to-vanilla/src/transform-keys/merge-key.ts
+++ packages/transform-to-vanilla/src/transform-keys/merge-key.ts
@@ -18,9 +18,15 @@ if (import.meta.vitest) {
     it("No Sign Symbol", () => {
       expect(removeSignSimbol("boxShadow")).toBe("boxShadow");
     });
-    it("Has Sign Symbol", () => {
+    it("Has Sign Symbol to the end", () => {
       expect(removeSignSimbol("boxShadow$")).toBe("boxShadow");
       expect(removeSignSimbol("transform_")).toBe("transform");
+    });
+    it("Has Sign Symbol in the middle or at the first", () => {
+      expect(removeSignSimbol("box$Shadow")).toBe("box$Shadow");
+      expect(removeSignSimbol("trans_form")).toBe("trans_form");
+      expect(removeSignSimbol("$boxShadow")).toBe("$boxShadow");
+      expect(removeSignSimbol("_transform")).toBe("_transform");
     });
   });
 }
