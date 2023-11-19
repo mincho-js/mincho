79f32b5c91528a4b8182e9308b1b775dcb1e736e
JeongJun <rgfdds98@gmail.com>
2023-11-19 16:54:33 +0900

    Feat: Remove Key Sign Symbol Function (#4)
    * Feat: create Remove Key Sign Symbol Fn with test code
    
    * Test: add test case about Remove Key Sign Symbol Fn
    
    * Fix: refine Types and TypeGuard Fn

---
+++ packages/transform-to-vanilla/src/transform-keys/merge-key.ts
@@ -0,0 +1,36 @@
+type DefaultType = string & NonNullable<unknown>;
+type HasSignInString = `${DefaultType}$` | `${DefaultType}_`;
+type InputKeyValue = DefaultType | HasSignInString;
+
+// == Interface ================================================================
+export function removeSignSimbol(keyStr: InputKeyValue) {
+  return hasSignSimbol(keyStr)
+    ? keyStr.substring(0, keyStr.length - 1)
+    : keyStr;
+}
+
+// == Utils ====================================================================
+function hasSignSimbol(value: InputKeyValue): value is HasSignInString {
+  return value.endsWith("$") || value.endsWith("_");
+}
+
+// == Tests ====================================================================
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe.concurrent("Remove Key Sign Symbol", () => {
+    it("No Sign Symbol", () => {
+      expect(removeSignSimbol("boxShadow")).toBe("boxShadow");
+    });
+    it("Has Sign Symbol to the end", () => {
+      expect(removeSignSimbol("boxShadow$")).toBe("boxShadow");
+      expect(removeSignSimbol("transform_")).toBe("transform");
+    });
+    it("Has Sign Symbol in the middle or at the first", () => {
+      expect(removeSignSimbol("box$Shadow")).toBe("box$Shadow");
+      expect(removeSignSimbol("trans_form")).toBe("trans_form");
+      expect(removeSignSimbol("$boxShadow")).toBe("$boxShadow");
+      expect(removeSignSimbol("_transform")).toBe("_transform");
+    });
+  });
+}
