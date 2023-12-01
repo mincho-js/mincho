ebc778465cadd9456d20a4eb59052592174eff2a
alstjr7375 <alstjr7375@daum.net>
2023-12-01 19:21:14 +0900

    Feat: Transform style initial implemented with simply important

--- packages/transform-to-vanilla/src/types/style-rule.ts
+++ packages/transform-to-vanilla/src/types/style-rule.ts
@@ -3,5 +3,10 @@ import type { ComplexStyleRule, StyleRule } from "@vanilla-extract/css";
 export type ComplexCSSRule = ComplexStyleRule;
 export type CSSRule = StyleRule;
 
+export type StyleRuleKey = keyof StyleRule;
+export type StyleRuleValue = StyleRule[StyleRuleKey];
+export type CSSRuleKey = keyof CSSRule;
+export type CSSRuleValue = CSSRule[CSSRuleKey];
+
 export type VanillaStyleArray = Exclude<ComplexStyleRule, StyleRule>;
 export type VanillaClassNames = Exclude<VanillaStyleArray[number], StyleRule>;
