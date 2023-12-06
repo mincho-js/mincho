531b08b33ca2f64b132bc00fd786d6923202a839
JeongJun <rgfdds98@gmail.com>
2023-12-06 23:52:12 +0900

    Test: update test code about SimplePseudoSelector Fn

--- packages/transform-to-vanilla/src/transform-keys/simple-pseudo-selectors.ts
+++ packages/transform-to-vanilla/src/transform-keys/simple-pseudo-selectors.ts
@@ -8,7 +8,7 @@ type ReturnKeyValue = NonNullableString | SimplePseudos;
 
 // == Interface ================================================================
 export function replacePseudoSelectors(keyStr: InputKeyValue): ReturnKeyValue {
-  const kebabKeyStr = camelToKebab(keyStr);
+  const kebabKeyStr = keyStr.startsWith("_") ? camelToKebab(keyStr) : keyStr;
 
   return hasSinglePseudoSelector(kebabKeyStr)
     ? `:${kebabKeyStr.substring(1, kebabKeyStr.length)}`
@@ -34,23 +34,46 @@ function hasDoublePseudoSelector(
 
 // == Tests ====================================================================
 if (import.meta.vitest) {
-  const { describe, it, expect } = import.meta.vitest;
+  const { describe, it, expect, expectTypeOf } = import.meta.vitest;
 
   describe.concurrent("Replace Simple Pseudo Selectors Sign", () => {
-    it("No Simple Pseudo Selectors Sign", () => {
+    it("No Simple Pseudo", () => {
       expect(replacePseudoSelectors("after")).toBe("after");
+      expect(replacePseudoSelectors("backgroundColor")).toBe("backgroundColor");
+      expect(replacePseudoSelectors(".myClassName > &")).toBe(
+        ".myClassName > &"
+      );
+      expect(replacePseudoSelectors("&:hover:not(:active)")).toBe(
+        "&:hover:not(:active)"
+      );
+      expect(replacePseudoSelectors("@supports (display: grid)")).toBe(
+        "@supports (display: grid)"
+      );
     });
-    it("Has Simple Pseudo Selectors Sign to the first", () => {
-      expect(replacePseudoSelectors("_hover")).toBe(":hover");
-      expect(replacePseudoSelectors("_firstOfType")).toBe(":first-of-type");
-      expect(replacePseudoSelectors("__before")).toBe("::before");
-      expect(replacePseudoSelectors("___before")).toBe("::_before");
+    it("Has Single or Double Simple Pseudo at the first", () => {
+      const singleSimplePseudo = replacePseudoSelectors("_hover");
+      expect(singleSimplePseudo).toBe(":hover");
+      expectTypeOf(singleSimplePseudo).toEqualTypeOf<ReturnKeyValue>();
+
+      const doubleSimplePseudo = replacePseudoSelectors("__before");
+      expect(doubleSimplePseudo).toBe("::before");
+      expectTypeOf(doubleSimplePseudo).toEqualTypeOf<ReturnKeyValue>();
+
+      const doubleSimplePseudoWithCamel =
+        replacePseudoSelectors("__firstOfType");
+      expect(doubleSimplePseudoWithCamel).toBe("::first-of-type");
+      expectTypeOf(doubleSimplePseudoWithCamel).toEqualTypeOf<ReturnKeyValue>();
     });
-    it("Has Simple Pseudo Selectors Sign in the middle or at the end", () => {
+    it("Has Triple Simple Pseudo at the first", () => {
+      const tripleSimplePseudo = replacePseudoSelectors("___active");
+      expect(tripleSimplePseudo).toBe("::_active");
+      expectTypeOf(tripleSimplePseudo).toEqualTypeOf<ReturnKeyValue>();
+    });
+    it("Has Simple Pseudo in the middle or at the end", () => {
       expect(replacePseudoSelectors("hover_")).toBe("hover_");
       expect(replacePseudoSelectors("ho_ver")).toBe("ho_ver");
-      expect(replacePseudoSelectors("firstOfType_")).toBe("first-of-type_");
-      expect(replacePseudoSelectors("first_OfType_")).toBe("first_-of-type_");
+      expect(replacePseudoSelectors("firstOfType_")).toBe("firstOfType_");
+      expect(replacePseudoSelectors("first_OfType_")).toBe("first_OfType_");
       expect(replacePseudoSelectors("before__")).toBe("before__");
       expect(replacePseudoSelectors("be_fore__")).toBe("be_fore__");
     });
