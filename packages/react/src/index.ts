bc7dd88d744fe292db499108074d2a06c49a2063
alstjr7375 <alstjr7375@daum.net>
2025-08-16 00:00:00 +0900

    Refactor: `styled` code order & naming

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -23,6 +23,148 @@ export { $$styled } from "./runtime.js";
 
 type KeyofIntrinsicElements = keyof JSX.IntrinsicElements;
 
+// == Main =====================================================================
+export function styled<
+  Props,
+  Component extends ForwardRefExoticComponent<Props>,
+  RulesVariants extends VariantGroups | undefined = undefined,
+  RulesToggleVariants extends VariantDefinitions | undefined = undefined,
+  RulesProps extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+>(
+  component: Component,
+  options: PatternOptions<RulesVariants, RulesToggleVariants, RulesProps>
+): StyledComponent<
+  Props,
+  Component,
+  RulesVariants,
+  RulesToggleVariants,
+  RulesProps
+>;
+
+export function styled<
+  Props,
+  Component extends ComponentType<Props>,
+  RulesVariants extends VariantGroups | undefined = undefined,
+  RulesToggleVariants extends VariantDefinitions | undefined = undefined,
+  RulesProps extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+>(
+  component: Component,
+  options: PatternOptions<RulesVariants, RulesToggleVariants, RulesProps>
+): StyledComponent<
+  Props,
+  Component,
+  RulesVariants,
+  RulesToggleVariants,
+  RulesProps
+>;
+
+export function styled<
+  Props extends object,
+  Component extends ElementType,
+  RulesVariants extends VariantGroups | undefined = undefined,
+  RulesToggleVariants extends VariantDefinitions | undefined = undefined,
+  RulesProps extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+>(
+  component: ComponentWithAs<Props, Component>,
+  options: PatternOptions<RulesVariants, RulesToggleVariants, RulesProps>
+): StyledComponent<
+  Props,
+  Component,
+  RulesVariants,
+  RulesToggleVariants,
+  RulesProps
+>;
+
+export function styled<
+  Component extends KeyofIntrinsicElements,
+  RulesVariants extends VariantGroups | undefined = undefined,
+  RulesToggleVariants extends VariantDefinitions | undefined = undefined,
+  RulesProps extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+>(
+  component: Component,
+  options: PatternOptions<RulesVariants, RulesToggleVariants, RulesProps>
+): StyledComponent<
+  IntrinsicProps<Component>,
+  Component,
+  RulesVariants,
+  RulesToggleVariants,
+  RulesProps
+>;
+
+export function styled<
+  Component extends
+    | ForwardRefExoticComponent<unknown>
+    | ComponentType<unknown>
+    | KeyofIntrinsicElements
+    | ComponentWithAs<object, ElementType>,
+  RulesVariants extends VariantGroups | undefined = undefined,
+  RulesToggleVariants extends VariantDefinitions | undefined = undefined,
+  RulesProps extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+>(
+  _component: Component,
+  _options: PatternOptions<RulesVariants, RulesToggleVariants, RulesProps>
+): StyledComponent<
+  InferStyledComponentProps<Component>,
+  InferStyledComponentElement<Component>,
+  RulesVariants,
+  RulesToggleVariants,
+  RulesProps
+> {
+  throw new Error(
+    "This function shouldn't be there in your final code. If you're seeing this, there is probably some issue with your build config."
+  );
+}
+
+type IntrinsicProps<TComponent> = TComponent extends KeyofIntrinsicElements
+  ? JSX.IntrinsicElements[TComponent]
+  : never;
+
+type InferStyledComponentProps<Component> =
+  Component extends KeyofIntrinsicElements
+    ? IntrinsicProps<Component>
+    : Component extends ComponentType<infer ComponentTypeProps>
+      ? ComponentTypeProps
+      : Component extends ForwardRefExoticComponent<infer ForwardRefProps>
+        ? ForwardRefProps
+        : Component extends ComponentWithAs<infer WithAsProps, ElementType>
+          ? WithAsProps
+          : never;
+
+type InferStyledComponentElement<Component> =
+  Component extends KeyofIntrinsicElements
+    ? Component
+    : Component extends ElementType
+      ? Component
+      : Component extends ComponentWithAs<infer _WithAsProps, infer AsComponent>
+        ? AsComponent
+        : never;
+
+// == Helpers ==================================================================
+export type StyledComponent<
+  TProps = Record<string, unknown>,
+  Component extends ElementType = ElementType,
+  Variants extends VariantGroups | undefined = undefined,
+  ToggleVariants extends VariantDefinitions | undefined = undefined,
+  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+> = ComponentWithAs<
+  TProps &
+    RefAttributes<unknown> &
+    PatternOptionsToProps<Variants, ToggleVariants, Props>,
+  Component
+>;
+
+export type PatternOptionsToProps<
+  Variants extends VariantGroups | undefined = undefined,
+  ToggleVariants extends VariantDefinitions | undefined = undefined,
+  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
+> = ResolveComplex<
+  NonNever<
+    VariantObjectSelection<ConditionalVariants<Variants, ToggleVariants>>
+  > &
+    NonNever<PropDefinitionOutput<Exclude<Props, undefined>>>
+>;
+type NonNever<T> = [T] extends [never] ? unknown : T;
+
 export type DefaultPropsType = Record<never, never>;
 export interface WithAsProp<AsComponent extends ElementType> {
   as?: AsComponent;
@@ -59,126 +201,4 @@ export interface ComponentWithAs<
    */
   displayName?: string;
   readonly $$typeof: symbol;
-}
-
-export type StyledComponent<
-  TProps = Record<string, unknown>,
-  Component extends ElementType = ElementType,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
-> = ComponentWithAs<
-  TProps &
-    RefAttributes<unknown> &
-    PatternOptionsToProps<Variants, ToggleVariants, Props>,
-  Component
->;
-
-export type PatternOptionsToProps<
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
-> = ResolveComplex<
-  NonNever<
-    VariantObjectSelection<ConditionalVariants<Variants, ToggleVariants>>
-  > &
-    NonNever<PropDefinitionOutput<Exclude<Props, undefined>>>
->;
-type NonNever<T> = [T] extends [never] ? unknown : T;
-
-type IntrinsicProps<TComponent> = TComponent extends KeyofIntrinsicElements
-  ? JSX.IntrinsicElements[TComponent]
-  : never;
-
-type InferStyledComponentProps<Component> =
-  Component extends KeyofIntrinsicElements
-    ? IntrinsicProps<Component>
-    : Component extends ComponentType<infer ComponentTypeProps>
-      ? ComponentTypeProps
-      : Component extends ForwardRefExoticComponent<infer ForwardRefProps>
-        ? ForwardRefProps
-        : Component extends ComponentWithAs<infer WithAsProps, ElementType>
-          ? WithAsProps
-          : never;
-
-type InferStyledComponentElement<Component> =
-  Component extends KeyofIntrinsicElements
-    ? Component
-    : Component extends ElementType
-      ? Component
-      : Component extends ComponentWithAs<infer _WithAsProps, infer AsComponent>
-        ? AsComponent
-        : never;
-
-export function styled<
-  TProps,
-  TComponent extends ForwardRefExoticComponent<TProps>,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
->(
-  component: TComponent,
-  options: PatternOptions<Variants, ToggleVariants, Props>
-): StyledComponent<TProps, TComponent, Variants, ToggleVariants, Props>;
-
-export function styled<
-  TProps,
-  TComponent extends ComponentType<TProps>,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
->(
-  component: TComponent,
-  options: PatternOptions<Variants, ToggleVariants, Props>
-): StyledComponent<TProps, TComponent, Variants, ToggleVariants, Props>;
-
-export function styled<
-  TProps extends object,
-  TComponent extends ElementType,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
->(
-  component: ComponentWithAs<TProps, TComponent>,
-  options: PatternOptions<Variants, ToggleVariants, Props>
-): StyledComponent<TProps, TComponent, Variants, ToggleVariants, Props>;
-
-export function styled<
-  TComponent extends KeyofIntrinsicElements,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
->(
-  component: TComponent,
-  options: PatternOptions<Variants, ToggleVariants, Props>
-): StyledComponent<
-  IntrinsicProps<TComponent>,
-  TComponent,
-  Variants,
-  ToggleVariants,
-  Props
->;
-
-export function styled<
-  Component extends
-    | ForwardRefExoticComponent<unknown>
-    | ComponentType<unknown>
-    | KeyofIntrinsicElements
-    | ComponentWithAs<object, ElementType>,
-  Variants extends VariantGroups | undefined = undefined,
-  ToggleVariants extends VariantDefinitions | undefined = undefined,
-  Props extends ComplexPropDefinitions<PropTarget> | undefined = undefined
->(
-  _component: Component,
-  _options: PatternOptions<Variants, ToggleVariants, Props>
-): StyledComponent<
-  InferStyledComponentProps<Component>,
-  InferStyledComponentElement<Component>,
-  Variants,
-  ToggleVariants,
-  Props
-> {
-  throw new Error(
-    "This function shouldn't be there in your final code. If you're seeing this, there is probably some issue with your build config."
-  );
 }
