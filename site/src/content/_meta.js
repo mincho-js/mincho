ef3d89ca6b3c5d80fe1dc4cf0a1e5435c2bc7f48
JeongJun <rgfdds98@gmail.com>
2025-11-30 21:59:42 +0900

    Docs: update getting-started and rename _meta.js to .ts
    - Rename _meta.js to _meta.ts for TypeScript consistency
    - Fix Vite plugin export name: mincho → minchoVitePlugin
    - Add @vanilla-extract/css to installation command
    - Expand getting-started with practical examples and key features

--- site/src/content/_meta.js
+++
@@ -1,4 +0,0 @@
-export default {
-  index: 'Introduction',
-  'getting-started': 'Getting Started'
-}
---
+++ site/src/content/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  index: "Introduction",
+  "getting-started": "Getting Started",
+  literal: "Literal CSS"
+};
